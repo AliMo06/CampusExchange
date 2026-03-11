e374c2d9977a6919f2c6a088c7a196880bcc909c
Shxdw-GX <senan.aboluhom2@gmail.com>
2026-03-11 00:06:02 -0400

    set up express server with routes and db connection

---
+++ Application/src/routes/auth.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+router.post('/register', (req, res) => {
+  res.json({ message: 'register route' })
+})
+
+router.post('/login', (req, res) => {
+  res.json({ message: 'login route' })
+})
+
+module.exports = router
